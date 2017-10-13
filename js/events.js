5ef224ee49b52791d83d606091cbd67508e2bc44
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-13 20:55:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,10 +1,8 @@
 //define functions here
 function getIt() {
   // set up event handler to pay attention to p tag
-  $('p').on('click',function(clicked){
-    if (clicked.which === true){
-      alert('Hey!');
-    }
+  $( "p" ).click(function() {
+    alert( "Hey!" );
   });
 }
 
