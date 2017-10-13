4f5585ad136aeb7184c8b3edb6014deec860a70d
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-13 21:06:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,7 +8,7 @@ function getIt() {
 
 function frameIt(){
 // bind the load event that adds the class tasty to the image to add a red frame to the image
-  $('img.tasty').on('load', function(){
+  $('img').on('load', function(){
   $(this).css('border', "solid 2px red");
 });
 }
