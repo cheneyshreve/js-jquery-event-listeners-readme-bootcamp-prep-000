8c1713d9ed19ede99c2a003c04d7ff0aa1c4ff38
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-13 20:52:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,19 @@
 //define functions here
+function getIt() {
+  // set up event handler to pay attention to p tag
+  $('p').on('click',function(clicked){
+    if (clicked.which === true){
+      alert('Hey!');
+    }
+  });
+}
+
+
 
 $(document).ready(function(){
 
 // call functions here
 
 });
+
+
